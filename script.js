6700950d712fd174ba48d0d4ce893e13826621bd
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-14 15:31:16 -0300

    Exercício de IF/WHILE do curso Vai na Web

---
+++ script.js
@@ -0,0 +1,19 @@
+for(let contando = 1; contando <= 10; contando += 1){
+  console.log(contando);
+}
+
+let contador = 12
+while (1<= contador) {
+  console.log(contador);
+  contador -=1;
+}
+
+for (let impares = 1; impares <=100; impares +=2){
+  console.log(impares)
+}
+
+let pares = 0
+while (100 >= pares){
+  console.log(pares);
+  pares +=2;
+}
